89d0ee373711a554451f385a06df90e4431d77eb
Galina <baleva1990@gmail.com>
2025-10-21 23:36:30 +0200

    Initialized the Nex.js project.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Hello from "@/app/components/hello";
+
+export default function Home() {
+    console.log("Home page");
+  return (
+      <>
+          HOME
+      </>
+     );
+}
