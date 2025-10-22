4927c3222e93eb8feb19f1a62b3599223f9b3e3b
Galina <baleva1990@gmail.com>
2025-10-22 10:51:17 +0200

    Add Navbar and GitHub - Authorizing OAuth Apps

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import Hello from "@/app/components/hello";
 
 export default function Home() {
     console.log("Home page");
   return (
       <>
-          HOME
+          <div>Home</div>
       </>
      );
 }
