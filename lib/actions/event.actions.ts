f04f8386e6dd41ea0b8cd034de9fbf51583a6db4
Galina <baleva1990@gmail.com>
2026-02-13 12:46:52 +0100

    Add getEventBySlug action

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -11,6 +11,30 @@ export async function getEvents(): Promise<IEvent[]> {
 
 
 export async function getEventBySlug(slug: string): Promise<IEvent | null> {
-  await connectDB();
-  return Event.findOne({ slug }).lean<IEvent>();
+  try {
+    await connectDB();
+    return Event.findOne({ slug }).lean<IEvent>();
+
+  } catch (error) {
+    console.error(`Error fetching event with slug ${slug}:`, error);
+    return null;
+  }
+}
+
+export async function getSimilarEvents(slug: string) {
+  try {
+    await connectDB();
+    const event = await Event.findOne({ slug });
+    if (!event) {
+      return [];
+    }
+    return Event.find({
+      _id: { $ne: event },
+      tags: { $in: event.tags },
+    }).lean<IEvent[]>();
+
+  } catch (error) {
+    console.error(`Error fetching similar events for slug ${slug}:`, error);
+    return [];
+  }
 }
